0c97b3e300ad872dea0e1d1fbb5d6f4798fa73e0
Val Sab <valerie.sabando@gmail.com>
2023-09-06 23:22:10 +0200

    Improve description in readme.md

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
+import { argv } from 'node:process';
 import chalk from 'chalk';
+import randomColor from 'randomcolor';
 
-// generates hex code
-let letters = '0123456789ABCDEF';
-
-let color = '#';
-
-for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
-
-console.log(color);
-
-// prints block of 31x9 # characters
-const character = '#';
-
-const width = 31;
-const height = 9;
-let block = '';
-
-for (let row = 0; row < height; row++) {
-  // Loop through each column
-  for (let col = 0; col < width; col++) {
-    // Concatenate "#" to the block
-    block += '#';
-  }
-  // Add a new line after each row
-  block += '\n';
-}
-console.log(block);
-
-// color the block and hex code
-const cc = require('node-console-colors');
-console.log(cc.set('color', 'block'));
-
-/*
-console.log(cc.set('fg_red', 'Hello World in red'));
-const cc = require('node-console-colors');
-
-console.log(cc.set('fg_red', 'Hello World in red'));
-
-console.log(cc.set('fg_blue', 'Hello World in blue'));
+const color = randomColor({ hue: argv[2], luminosity: argv[3] });
 
 console.log(
-  cc.set(
-    'fg_green',
-    'bg_dark_grey',
-    'Hello World',
-    'in fg_green and bg_dark_gray',
-  ),
+  chalk.hex(color).bold(`###############################
+###############################
+###############################
+#####                     #####
+#####       ${color}       #####
+#####                     #####
+###############################
+###############################
+###############################`),
 );
